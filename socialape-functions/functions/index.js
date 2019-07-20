b86a28af23928291a64f693dde0a2e64d105ef8a
RomanBogatikov <ivaroman2000@gmail.com>
2019-07-20 14:01:49 -0400

    added createScream function to write a new scream to Firebase Cloud database

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -10,6 +10,36 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello world!");
 });
 
-exports.getScreams = function.https.onRequest((req, res) => {
-  admin.firestore().collection('')
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach( doc => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err))
+})
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully`})
+    })
+    .catch(err => {
+      res.status(500).json({ error: `something went wrong`});
+      console.error(err);
+    })
 })
